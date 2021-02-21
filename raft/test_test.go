f77c17ced20eaa0b47c1b9d17f44111ca97d5c9e
shoaibCS <19100080@lums.edu.pk>
2021-02-21 17:06:23 -0600

    Added some debug statememts in test_test.go

--- raft/test_test.go
+++ raft/test_test.go
@@ -56,32 +56,43 @@ func TestReElection2A(t *testing.T) {
 	defer cfg.cleanup()
 
 	cfg.begin("Test (2A): election after network failure")
-
+    fmt.Printf("1: cfg.checkOneLeader()")
 	leader1 := cfg.checkOneLeader()
+    fmt.Printf("1: done")
 
 	// if the leader disconnects, a new one should be elected.
+    fmt.Printf("2: if the leader disconnects, a new one should be elected.")
 	cfg.disconnect(leader1)
 	cfg.checkOneLeader()
+    fmt.Printf("2: done")
 
 	// if the old leader rejoins, that shouldn't
 	// disturb the new leader.
+    fmt.Printf("3: if the old leader rejoins, that shouldn't, disturb the new leader.")
 	cfg.connect(leader1)
 	leader2 := cfg.checkOneLeader()
+    fmt.Printf("3: done")
 
 	// if there's no quorum, no leader should
 	// be elected.
+    fmt.Printf("4: if there's no quorum, no leader should be elected")
 	cfg.disconnect(leader2)
 	cfg.disconnect((leader2 + 1) % servers)
 	time.Sleep(2 * RaftElectionTimeout)
 	cfg.checkNoLeader()
-
-	// if a quorum arises, it should elect a leader.
+    fmt.Printf("4: done")
+    
+    // if a quorum arises, it should elect a leader.
+    fmt.Printf("5:  if a quorum arises, it should elect a leader.")
 	cfg.connect((leader2 + 1) % servers)
 	cfg.checkOneLeader()
+    fmt.Printf("5: done")
 
 	// re-join of last node shouldn't prevent leader from existing.
+    fmt.Printf("6: re-join of last node shouldn't prevent leader from existing.")
 	cfg.connect(leader2)
 	cfg.checkOneLeader()
+    fmt.Printf("6: done")
 
 	cfg.end()
 }
